92c477d4bb693c1e7900ab897749bf8fb587ea53
이하늘 <wsxedc901@gmail.com>
2022-11-07 23:13:24 +0900

    Main.js 변경

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,3 +1,24 @@
-export function Main() {
-  return <div>Hello World!</div>;
+import styled from 'styled-components';
+
+export default function Main() {
+  return (
+    <>
+      <nav>
+        <ul>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+        </ul>
+      </nav>
+      <Section></Section>
+      <Section></Section>
+      <Section></Section>
+      <Section></Section>
+    </>
+  );
 }
+
+const Section = styled.div`
+  height: 100vh;
+`;
